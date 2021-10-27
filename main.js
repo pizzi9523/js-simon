75f705388ba034cd4d4f65b5160a64a0c5cd02a7
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-27 16:17:35 +0200

    BONUS numeri a schermo, timer, button start/restar
    inserisco un button che fa partire il timer e rendo il timer visibile

--- main.js
+++ main.js
@@ -9,29 +9,68 @@ while (numbers.length < 5) {
 }
 
 //console.log(numbers);
-alert(numbers)
+const containerElement = document.querySelector(".container")
 
-let right_numbers = []
-let counter = 0;
-setTimeout(function () {
-    for (let i = 0; i < 5; i++) {
-        const user_choice = parseInt(prompt("Inserisci un numero"))
-        if (numbers.includes(user_choice) && !right_numbers.includes(user_choice)) {
-            right_numbers.push(user_choice)
-            counter++
+
+numbers.forEach(number => {
+    containerElement.insertAdjacentHTML("beforeend", ` ${number} `)
+})
+
+
+
+
+
+document.getElementById("restart").addEventListener("click", restartFunction);
+
+function restartFunction() {
+    console.log("cliccato");
+    this.style.display = "none"
+    containerElement.innerHTML = ""
+    timer();
+
+    let right_numbers = []
+    let counter = 0;
+    setTimeout(function () {
+        for (let i = 0; i < 5; i++) {
+            const user_choice = parseInt(prompt("Inserisci un numero"))
+            if (numbers.includes(user_choice) && !right_numbers.includes(user_choice)) {
+                right_numbers.push(user_choice)
+                counter++
+            }
         }
-    }
-    // console.log(right_numbers);
-    /* right_numbers.forEach(number => {
-        console.log(number);
-    }) */
-    //console.log(counter);
-    console.log(`Hai indovintato ${counter} numeri e sono ${right_numbers}`);
-    document.querySelector(".container").innerHTML = `Hai indovintato ${counter} numeri`;
-    if (counter > 0) {
-        document.querySelector(".container").insertAdjacentHTML("beforeend", `e sono: `)
-        right_numbers.forEach(number => {
-            document.querySelector(".container").insertAdjacentHTML("beforeend", ` ${number} `)
-        })
-    }
-}, 30000)
+        // console.log(right_numbers);
+        /* right_numbers.forEach(number => {
+            console.log(number);
+        }) */
+        //console.log(counter);
+        //console.log(`Hai indovintato ${counter} numeri e sono ${right_numbers}`);
+        document.querySelector(".container").innerHTML = `Hai indovintato ${counter}`;
+        if (counter > 1) {
+            document.querySelector(".container").insertAdjacentHTML("beforeend", ` numeri e sono: `)
+            right_numbers.forEach(number => {
+                document.querySelector(".container").insertAdjacentHTML("beforeend", ` ${number} `)
+            })
+        } else if (counter == 1) {
+            document.querySelector(".container").insertAdjacentHTML("beforeend", ` numero ed è: `)
+
+            document.querySelector(".container").insertAdjacentHTML("beforeend", ` ${right_numbers} `)
+
+        }
+        document.getElementById("restart").style.display = "block"
+        document.getElementById("restart").innerHTML = "Restart"
+
+
+    }, 31000)
+
+}
+
+function timer() {
+    let seconds = 0;
+    let clock = setInterval(() => {
+        seconds++
+        document.querySelector(".container").innerHTML = seconds
+        if (seconds == 30) {
+            clearInterval(clock)
+        }
+    }, 1000);
+}
