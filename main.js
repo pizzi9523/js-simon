9b9feb18d1e00d2c0a671a2429692153a0ec8f06
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-27 15:36:10 +0200

    imposto un timer per indovinare i numeri
    imposto un timer di 30 secondi e mostro a schermo quanti e quali numeri

--- main.js
+++ main.js
@@ -8,6 +8,30 @@ while (numbers.length < 5) {
     }
 }
 
+//console.log(numbers);
 alert(numbers)
 
-
+let right_numbers = []
+let counter = 0;
+setTimeout(function () {
+    for (let i = 0; i < 5; i++) {
+        const user_choice = parseInt(prompt("Inserisci un numero"))
+        if (numbers.includes(user_choice) && !right_numbers.includes(user_choice)) {
+            right_numbers.push(user_choice)
+            counter++
+        }
+    }
+    // console.log(right_numbers);
+    /* right_numbers.forEach(number => {
+        console.log(number);
+    }) */
+    //console.log(counter);
+    console.log(`Hai indovintato ${counter} numeri e sono ${right_numbers}`);
+    document.querySelector(".container").innerHTML = `Hai indovintato ${counter} numeri`;
+    if (counter > 0) {
+        document.querySelector(".container").insertAdjacentHTML("beforeend", `e sono: `)
+        right_numbers.forEach(number => {
+            document.querySelector(".container").insertAdjacentHTML("beforeend", ` ${number} `)
+        })
+    }
+}, 30000)
