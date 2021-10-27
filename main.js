0450002ae3fb94f51f37a115c67a60676d20abf6
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-27 14:47:21 +0200

    creo 5 numeri casuali e li mostro in alert

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+let numbers = []
 
+let i = 0;
+while (numbers.length < 5) {
+    const randomNumber = Math.floor(Math.random() * 20)
+    if (!numbers.includes(randomNumber)) {
+        numbers.push(randomNumber)
+    }
+}
+
+alert(numbers)
 
 
